0a115df4ba3e07a35783d5ef89e25c379238972e
yrasik <stepanenkoyra@gmail.com>
2020-12-13 23:33:32 +0300

    Add files via upload

--- Sink_Lua_00.lua
+++ Sink_Lua_00.lua
@@ -18,10 +18,15 @@
 ]==]
 
 local file
+local self_name = 'Sink_Lua_00.lua'
 local name = "example_00"
 
 function initialize ()
   file = io.open(name..".dat", "w")
+  if( file == nil ) then
+    print('ERROR: '..self_name..': initialize:  file == nil')
+    return
+  end
   file:write(string.format("#TIME\tsig_from_gain\tsig_filters1_out\n"))
 end
 
@@ -39,11 +44,47 @@ end
 function destructor( )
   file:close()
   
-  file_dem = io.open(name..".dem", "w")
-  file_dem:write("plot '"..name..".dat' with lines\n")
-  file_dem:write("pause -1 \"Hit return to continue\"\n")
+  local pattern_gnuplot =
+[====[rem=rem --[[--lua
+@echo off
+set PATH_TO_GNUPLOT="c:\Program Files\gnuplot\bin"
+set PATH=%PATH_TO_GNUPLOT%;%PATH%
+@setlocal&  set luafile="%~f0" & if exist "%~f0.bat" set luafile="%~f0.bat"
+%CD%/../../../systemc/lua-5.3.5/bin/lua53  %luafile% %*& pause ]]  
+
+local gnuplot_dem =
+[[
+# gnuplot script:
+set xlabel "__x_label__"
+set ylabel "__y_label__"
+set key title "__title__"
+set mytics (1) 
+set grid xtics ytics mytics 
+
+plot \
+'__name__.dat' u 1:2   with lines   lw 2   title '__title_12__',\
+'__name__.dat' u 1:3   with lines   lw 2   title '__title_13__'
+
+pause -1 "Hit return to continue"
+]]
+
+file_dem = io.open("__name__.dem", "w")
+file_dem:write(gnuplot_dem)
+file_dem:close()
+
+os.execute('gnuplot "__name__.dem" ')
+
+]====]  
+
+  local gnuplot = pattern_gnuplot:gsub('__name__', name)
+  local gnuplot = gnuplot:gsub('__x_label__', 'Time, ñ')
+  local gnuplot = gnuplot:gsub('__y_label__', 'Voltage, Â')
+  local gnuplot = gnuplot:gsub('__title__', 'sin(x) and cos(x)')
+  local gnuplot = gnuplot:gsub('__title_12__', 'sin(x)')
+  local gnuplot = gnuplot:gsub('__title_13__', 'cos(x)') 
+  
+  file_dem = io.open(name..".bat", "w")
+  file_dem:write(gnuplot)
   file_dem:close()
-    
-  os.execute('gnuplot "'..name..'.dem" ')
 end
 
